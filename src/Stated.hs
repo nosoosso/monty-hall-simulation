4c080acfd893dcba9310e282fd7be5e6847c7d1b
nosoosso <nosoosso.null@gmail.com>
2019-04-20 14:15:15 +0900

    Fix

--- src/Stated.hs
+++ src/Stated.hs
@@ -32,19 +32,19 @@ play1000Times = forM [0 .. 999] $ \_ -> play
 
 play :: RandomState Game
 play = do
-  -- Step0: Put a car and goats behind the door
-  carDoor <- randomDoor
-  let step0 = Game {car = carDoor, firstChoice = Nothing, openedDoor = Nothing, secondChoice = Nothing}
-  selectedDoor <- randomDoor
-      -- Step1: Player select the door
-  let step1 = step0 {firstChoice = Just selectedDoor}
-      -- Step2: Open the door which has a goat
-  let goatDoor = fromJust $ find (\x -> x /= car step1 && x /= fromJust (firstChoice step1)) doors
-  let step2 = step1 {openedDoor = Just goatDoor}
-      -- Step3: (Maybe) change the door
-  let second = fromJust $ find (\x -> x /= fromJust (firstChoice step2) && x /= fromJust (openedDoor step2)) doors
-  let step3 = step2 {secondChoice = Just second}
-  return step3
+    -- Step0: Put a car and goats behind the door
+      carDoor <- randomDoor
+      let step0 = Game {car = carDoor, firstChoice = Nothing, openedDoor = Nothing, secondChoice = Nothing}
+    -- Step1: Player select the door
+      selectedDoor <- randomDoor
+      let step1 = step0 {firstChoice = Just selectedDoor}
+    -- Step2: Open the door which has a goat
+      let goatDoor = fromJust $ find (\x -> x /= car step1 && x /= fromJust (firstChoice step1)) doors
+      let step2 = step1 {openedDoor = Just goatDoor}
+    -- Step3: (Maybe) change the door
+      let second = fromJust $ find (\x -> x /= fromJust (firstChoice step2) && x /= fromJust (openedDoor step2)) doors
+      let step3 = step2 {secondChoice = Just second}
+      return step3
 
 randomDoor :: RandomState Door
 randomDoor = do
